f423fce59e6b5bce5935035d63bc3c5e9803052b
Dharmesh Wagh <wagh.tuitionclasses24@gmail.com>
2026-07-22 00:02:39 +0530

    Update assessment-config.js

--- assets/js/assessment-config.js
+++ assets/js/assessment-config.js
@@ -1,3 +1,3 @@
 const WTC_ASSESSMENT_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwewOj4Kp8veGTz_2SExtutMA-JPlLsLlqmPdIdPxWa5JSidMXnFR7ue3F0lCaXqmAkkg/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwvJi4X6VlazCWaRyms9Q-PgtBgN2u97NQZTKAxUJJYG6y1yT7qMU9DNpm_J4OUFYsFTA/exec'
 };
